b7fcda924e027d23503091d29aa1781db5af5809
Hemavathy J <jhemavathyj@gmail.com>
2025-01-30 21:48:39 +0530

    change the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import '../../src/App.css'
-const Home =()=>{
-    return(
-        <div className='home'>
-            <h1 className='heading'>WELCOME TO THE ATTENDENCE TRACKER PAGE..!</h1>
-            <img src="https://myattendancetracker.com/images/frontend/tentered-theme/math-student.jpg" alt="" />
-        </div>
-    )
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../css/Home.css';
+
+const Home = () => {
+    const navigate = useNavigate();
+    const handleClassClick = (className) => {
+        navigate("/markattedence", { state: { className } });
+    };
+
+    return (
+        <section className="home-section">
+            <h1 className="home-heading">WELCOME TO THE ATTENDANCE TRACKER PAGE..!</h1>
+            <div className="classes-container">
+                <div className="class-box class-1" onClick={() => handleClassClick('class-1')}>CSE A</div>
+                <div className="class-box class-2" onClick={() => handleClassClick('class-2')}>CSE B</div>
+                <div className="class-box class-3" onClick={() => handleClassClick('class-3')}>CSE C</div>
+                <div className="class-box class-4" onClick={() => handleClassClick('class-4')}>IT</div>
+                <div className="class-box class-5" onClick={() => handleClassClick('class-5')}>CCE</div>
+            </div>
+        </section>
+    );
 }
 
-export default Home
+export default Home;
